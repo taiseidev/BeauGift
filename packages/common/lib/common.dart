7103c5d1de3ca84001074ec18eb74a45736fe372
onishi taisei <taisei.developer@gmail.com>
2023-09-29 18:52:16 +0900

    Refactor Common class for platform-specific initialization

--- packages/common/lib/common.dart
+++ packages/common/lib/common.dart
@@ -1,10 +1,32 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class Common {
   const Common._();
 
-  static Future<void> initializeDb() async {
+  static Future<void> init() async {
+    _checkPlatform();
+    await _initializeDb();
+  }
+
+  static void _checkPlatform() {
+    const appType = String.fromEnvironment('APP_TYPE');
+
+    if (appType == "mobile") {
+      if (kIsWeb) {
+        throw Exception('This app is only supported on ios or android.');
+      }
+    }
+
+    if (appType == "admin") {
+      if (!kIsWeb) {
+        throw Exception('This app is only supported on web.');
+      }
+    }
+  }
+
+  static Future<void> _initializeDb() async {
     const envFile = String.fromEnvironment('env');
     await dotenv.load(fileName: envFile);
 
--- packages/mobile/lib/main.dart
+++ packages/mobile/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Common.initializeDb();
+  await Common.init();
   runApp(const MyApp());
 }
 
