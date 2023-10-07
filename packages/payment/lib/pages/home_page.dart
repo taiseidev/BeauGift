b0ea851369b1ebe766a384c1220ae8bde64c9644
onishi taisei <taisei.developer@gmail.com>
2023-10-07 22:52:32 +0900

    Refactor home page to make a POST request and open a URL

--- packages/payment/lib/pages/home_page.dart
+++ packages/payment/lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
 import 'dart:html';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({Key? key})
@@ -15,8 +18,78 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text('QR Data')),
       body: Center(
-        child: Text('Data from QR: $data'),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                await fetchAndOpenUrl();
+              },
+              child: const Text("PayPayで支払う"),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Future<void> fetchAndOpenUrl() async {
+    const apiUrl =
+        'https://us-central1-beau-gift-dev.cloudfunctions.net/api/paypay/oneTapCall';
+
+    // ヘッダーの設定
+    final headers = {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Your-User-Agent-Here'
+    };
+
+    // リクエストのbody
+    final body = json.encode({
+      'amount': 2000,
+    });
+
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: headers,
+      body: body,
+    );
+
+    final responseBody = json.decode(response.body);
+
+    final url = responseBody['url'];
+    print(url);
+
+    if (await canLaunchUrl(Uri.parse(url as String))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      print('Could not launch $url');
+    }
+    // if (response.statusCode == 200) {
+    // } else {
+    //   print('Error occurred: ${response.statusCode}');
+    // }
+  }
+
+  Future<void> fetchApi() async {
+    const apiUrl =
+        'https://us-central1-beau-gift-dev.cloudfunctions.net/api/paypay/oneTapCall';
+
+    // ヘッダーの設定
+    final headers = {
+      'Content-Type': 'application/json',
+      'User-Agent': 'Your-User-Agent-Here'
+    };
+
+    // リクエストのbody
+    final body = json.encode({
+      'amount': 2000,
+    });
+
+    final response = await http.post(
+      Uri.parse(apiUrl),
+      headers: headers,
+      body: body,
+    );
+
+    print(response.body);
   }
 }
