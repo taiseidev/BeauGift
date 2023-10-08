bad6e9d0e9a87070803ae3472d9c77ef5f51b44b
onishi taisei <taisei.developer@gmail.com>
2023-10-08 17:52:31 +0900

    Disable debug mode banner for PaymentPackage

--- packages/payment/lib/app.dart
+++ packages/payment/lib/app.dart
@@ -7,6 +7,7 @@ class PaymentPackage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: TipSelectionPage(),
     );
   }
