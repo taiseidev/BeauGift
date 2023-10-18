acafacc9e094122258aadd73c4c5ae8cf73afb61
onishi taisei <taisei.developer@gmail.com>
2023-10-18 23:27:47 +0900

    🚀 feat: デフォルトのフォントを設定

--- packages/mobile/lib/main.dart
+++ packages/mobile/lib/main.dart
@@ -19,6 +19,9 @@ class MyApp extends ConsumerWidget {
 
     return MaterialApp.router(
       title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'NotoSansJP',
+      ),
       routerConfig: router,
     );
   }
